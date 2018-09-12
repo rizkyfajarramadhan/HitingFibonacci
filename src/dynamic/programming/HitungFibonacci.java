97a41a6f8f4fc5dd778bd8247203eec20b851f1a
Rizky Fajar <Rizky Fajar@Dylixe>
2018-09-12 20:21:01 +0700

    Initial Commit

---
+++ src/dynamic/programming/HitungFibonacci.java
@@ -0,0 +1,19 @@
+package dynamic.programming;
+
+public class HitungFibonacci {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");
+    }
+
+    public static void main(String[] args) {
+        String identitas = "Rizky Fajar Ramadhan / XRPL4 / 32";
+        
+        tampilJudul(identitas);
+        
+        
+    }
+    
+}
